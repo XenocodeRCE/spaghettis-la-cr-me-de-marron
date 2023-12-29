ee1077841c29ea81251317f9b181fee52780e88c
Mr Roux <XenocodeRCE@users.noreply.github.com>
2023-12-29 16:02:34 +0100

    Update content.js
    fonctionne même pour le contenu non textuel (podcast, video etc)

--- content.js
+++ content.js
@@ -4,15 +4,20 @@ function modifyHTML() {
   console.log('Content script is running on https://www.annabac.com/');
   console.log('Replacing partial content');
 
-  const elements = document.querySelectorAll('.inner-content[data-nosnippet][style*="max-height:"]');
-
+ // Modify elements with class "partial-content inner-content" and optional style "max-height"
+  const elements = document.querySelectorAll('.partial-content.inner-content[data-nosnippet]');
   elements.forEach((element) => {
     // 1. Remove "partial-content" from the class
     element.classList.remove('partial-content');
 
-    
-    // 2. Set max-height to 10000
-    element.style.setProperty('max-height', '10000px', 'important');
+    // 2. Check if max-height style is present
+    if (element.style.maxHeight) {
+      // If present, set max-height to 10000
+      element.style.setProperty('max-height', '10000px', 'important');
+    } else {
+      // If not present, add style max-height: 10000px
+      element.style.cssText += 'max-height: 10000px !important;';
+    }
   });
 
 
